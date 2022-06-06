ab319d18031eb4ddffd7fddba23e66439c9c1c10
FLP <DisturbReality.mm@gmail.com>
2022-06-06 18:53:00 +0200

    added error handling for 404 status

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,20 @@ const port = 3030;
 //server instance
 const app: Application = express();
 
+//middleware to parse the requests
+app.use(express.json());
+
 //main endpoint route
 app.get('/', (_req: Request, res: Response) => {
   res.send('Welcome to the second project');
+});
+
+app.use((_req: Request, res: Response) => {
+  res
+    .status(404)
+    .send(
+      'route not found, please read the documentation for available routes'
+    );
 });
 
 //express server start
