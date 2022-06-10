58ab9bfa3ad691150183a3a6ac72bd36773da057
FLP <42muhammedmustafa@gmail.com>
2022-06-10 17:55:25 +0200

    added migrations

--- migrations/sqls/20220609134526-users-table-up.sql
+++ migrations/sqls/20220609134526-users-table-up.sql
@@ -1,1 +1,8 @@
 /* Replace with your SQL commands */
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(50) NOT NULL
+);
